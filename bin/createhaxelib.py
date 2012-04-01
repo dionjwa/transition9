ac7a5accbfa7e4b0cce42c5a1f3d086cd0aea11f
Dion Amago <dion@transition9.com>
2012-03-31 19:12:38 -0700

    Haxelib submission script fixes.

--- bin/createhaxelib.py
+++ bin/createhaxelib.py
@@ -3,57 +3,73 @@
 #Creates the haxelib package
 import os, os.path, string, sys, shutil, tempfile, re, zipfile
 from xml.dom import minidom
+import argparse
 
-if len(sys.argv) <= 1:
-	print "Usage: createhaxelib.py <haxelib.etc> [srcFolder1] [srcFolder2, ...]"
-	sys.exit(0)
-
-etcFile = sys.argv[1]
-srcFolders = sys.argv[2:] if len (sys.argv) > 2 else None
-if not srcFolders:
-	srcFolders = [os.getcwd()]
-
-print "srcFolders=", srcFolders
-if not os.path.exists(etcFile):
-	print etcFile + " doesn't exist."
-	sys.exit(0)
-
-
-dom = minidom.parse(etcFile)
-projectName = dom.documentElement.attributes["name"].value.strip()
-print "Project name: " + projectName
-
-for srcFolder in srcFolders:
-	if not os.path.exists(srcFolder):
-		print srcFolder + " doesn't exist."
+def main (haxelibPath, srcFolders, nonSrcFolders, isLive):
+	
+	if not os.path.exists(haxelibPath):
+		print haxelibPath + " doesn't exist."
 		sys.exit(0)
-
-tmpfolder = tempfile.mkdtemp()
-
-def nodot (item):
-	return item[0] != '.'
-
-for src in srcFolders:
-	for f in filter(nodot, os.listdir(src)):
+	
+	dom = minidom.parse(haxelibPath)
+	projectName = dom.documentElement.attributes["name"].value.strip()
+	print "Project name: " + projectName
+	
+	tmpfolder = tempfile.mkdtemp()
+	
+	def nodot (item):
+		return item[0] != '.'
+	
+	if srcFolders != None:
+		for src in srcFolders:
+			if not os.path.exists(src):
+				continue
+			print "Adding", src
+			addFolder(src, tmpfolder, True);
+	
+	if nonSrcFolders != None:
+		for src in nonSrcFolders:
+			if not os.path.exists(src):
+				continue
+			print "Adding", src
+			addFolder(src, tmpfolder, False);
 		
+	shutil.copyfile(haxelibPath, os.path.join(tmpfolder, os.path.basename(haxelibPath)))
+	zipFileName = os.path.join("/tmp", projectName + ".zip")
+	z = zipfile.ZipFile(zipFileName, "w")
+	
+	for root, dirnames, files in os.walk(tmpfolder, followlinks=True):
+		for file in files:
+			if file != projectName + ".zip":
+				z.write(os.path.join(root, file), os.path.join(root, file).replace(tmpfolder, ""), zipfile.ZIP_DEFLATED )
+	z.close()
+	
+	if isLive:
+		command = "haxelib submit " + zipFileName
+	else:
+		command = "haxelib test " + zipFileName
+	print command
+	os.system(command)
+	shutil.rmtree(tmpfolder)
+	os.remove(zipFileName)
+
+def addFolder(src, dest, isSource=False):
+	if not isSource:
+		dest = os.path.join(dest, os.path.basename(src))
+	def nodot (item):
+		return item[0] != '.'
+	for f in filter(nodot, os.listdir(src)):
 		fullfilepath = os.path.join(src, f);
 		if os.path.isdir(fullfilepath):
-			shutil.copytree(fullfilepath, os.path.join(tmpfolder, f), ignore=shutil.ignore_patterns(".DS_Store", ".git", ".svn", "*.xml", "*.zip", "*.sh"))
-		elif re.match(".*(haxelib\.xml|\.hx|\.hxml|\.js)$", f):
-			shutil.copyfile(fullfilepath, os.path.join(tmpfolder, f))
-			
-shutil.copyfile(etcFile, os.path.join(tmpfolder, os.path.basename(etcFile)))
+			shutil.copytree(fullfilepath, os.path.join(dest, f), ignore=shutil.ignore_patterns(".DS_Store", ".git", ".svn", "*.xml", "*.zip", "*.sh", "*.hg", "build"	))
+		elif re.match(".*(haxelib\.xml|\.hx|\.hxml|\.js|\.nmml|\.png)$", f):
+			shutil.copyfile(fullfilepath, os.path.join(dest, f))
 
-zipFileName = os.path.join(tmpfolder, projectName + ".zip")
-z = zipfile.ZipFile(zipFileName, "w")
-
-for root, dirnames, files in os.walk(tmpfolder, followlinks=True):
-	for file in files:
-		if file != projectName + ".zip":
-			print file
-			z.write(os.path.join(root, file), os.path.join(root, file).replace(tmpfolder, ""), zipfile.ZIP_DEFLATED )
-z.close()
-
-shutil.move(zipFileName, os.path.join(os.getcwd(), projectName + ".zip"));
-shutil.rmtree(tmpfolder)
-
+if __name__=="__main__":
+	parser = argparse.ArgumentParser(description='Package and submit a haxelib package')
+	parser.add_argument('--src', action='append', default=["src"], help='A src folder (base folder is ignored, only *.hx files added)')
+	parser.add_argument('--extra', action='append', default=["demo", "demos"], help='An additional folder to add (base folder included, all files added')
+	parser.add_argument('--live', default=False, action="store_true", help='Without this, installs locally only')
+	parser.add_argument( '--haxelib', default="etc/haxelib.xml", help='Path to the haxelib.xml file')
+	namespace = parser.parse_args()
+	main(namespace.haxelib, namespace.src, namespace.extra, namespace.live)
